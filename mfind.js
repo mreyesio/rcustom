661dc996b2f610f4d08c9615daa1fc7f0c226abb
mreyesio <34176824+mreyesio@users.noreply.github.com>
2018-01-04 15:11:20 -0500

    Update mfind.js

--- mfind.js
+++ mfind.js
@@ -1,18 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost/customhomes";
 
-//find
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   db.collection("homes").find({}).toArray(function(err, result) {
-//     if (err) throw err;
-    
-//     console.log(result);
-//     db.close();
-//   });
-// });
-//seacrch
-
 
 module.exports = {
   
@@ -24,17 +12,6 @@ module.exports = {
       if (err) throw err;
       console.log("in mongo")
       rString[0]="dfsdf";
-      
-  
-    //   var query = { id: id_num };
-    //   db.collection("homes").find(query).toArray(function(err, result) {
-    //   if (err) throw err;
-    //   var parseDb=eval(result[0]);
-    //   rString=parseDb.zid;
-    //   db.close();
-    //   return "hehelhl"; 
-    //   });
-    // });
     
   });
   return rString[0];
@@ -42,44 +19,5 @@ module.exports = {
   }
   
 
-
- 
-// module.exports = class findID {
-//   id_num(ID) {
-  
-//   console.log("now in find js");
-  
-//   MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     var query = { id: ID };
-//     db.collection("homes").find(query).toArray(function(err, result) {
-//       if (err) throw err;
-//       var parseDb=eval(result[0]);
-//       rString=parseDb.zid;
-//       db.close();
-//     return rString;
-//     });
-//   });
-  
-// }
-
-// }
-// exports.findID = function (ID) {
-  
-//   console.log("now in find js");
-  
-//   MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     var query = { id: ID };
-//     db.collection("homes").find(query).toArray(function(err, result) {
-//       if (err) throw err;
-//       var parseDb=eval(result[0]);
-//       rString=parseDb.zid;
-//       db.close();
-     
-//     });
-//   });
-//   return rString;
-// };
 
 
