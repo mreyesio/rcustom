10384e926478fd663268cac18a9a6679d733cf78
mreyesio <34176824+mreyesio@users.noreply.github.com>
2018-01-04 15:12:23 -0500

    Update zillow_api.js

--- zillow_api.js
+++ zillow_api.js
@@ -24,22 +24,3 @@ module.exports = {
     }
 }
 
-// var myPythonScriptPath = 'homes.py';
-
-// // Use python shell
-// var PythonShell = require('python-shell');
-// var pyshell = new PythonShell(myPythonScriptPath);
-
-// pyshell.on('message', function (message) {
-//     // received a message sent from the Python script (a simple "print" statement)
-//     console.log(message);
-// });
-
-// // end the input stream and allow the process to exit
-// pyshell.end(function (err) {
-//     if (err){
-//         throw err;
-//     };
-
-//     console.log('finished');
-// });
